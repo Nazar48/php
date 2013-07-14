1a600de3cdfc1957a9f141f61a8a4ab81cfbac2b
Ivan Dives <ivan.dives@gmail.com>
2013-07-14 14:30:34 +0300

    add examples/sql.php

---
+++ examples/sql.php
@@ -0,0 +1,18 @@
+<?php
+
+function query_db($query) {
+	if (!mysql_connect('localhost', 'user', 'pass')) {
+		_error('could not connect');
+	}
+	if (!mysql_select_db('mad') {
+		_error('could not select db');
+	}
+	$result = mysql_query($query);
+	mysql_close();
+
+	return $result;
+}
+
+$data = query_db('select * from products where category="Аминокислоты"');
+
+?>
