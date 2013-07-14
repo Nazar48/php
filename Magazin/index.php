807e4b43e3248c9164e87b7fdc2003ab7530ef29
Ivan Dives <ivan.dives@gmail.com>
2013-07-14 20:57:15 +0300

    Added directory structure. Menu autogeneration.

---
+++ Magazin/index.php
@@ -0,0 +1,34 @@
+<?php
+
+require_once('includes/config.inc');
+
+//function _error($s) {
+//	mail('заказчик@mail.ru', 'Ошибка на сайте.', 'На сайте произошла ошибка. Свяжитесь с разработчиком.');
+//	error_log($s);
+//}
+
+function query_db($query) {
+	if (!mysql_connect()) {
+		error_log('DEBUG: could not connect to db: ' . mysql_error() . "\n");
+	}
+	if (!mysql_select_db('mad')) {
+		error_log('DEBUG: could not select db: ' . mysql_error() . "\n");
+	}
+	$result = mysql_query($query);
+	mysql_close();
+
+	return $result;
+}
+
+$resource = query_db('select distinct category from products');
+
+$sorted_categories = array();
+
+while ($row = mysql_fetch_assoc($resource)) {
+	array_push($sorted_categories, $row['category']);
+}
+sort($sorted_categories, SORT_STRING);
+
+require_once('templates/index.tmpl');
+
+?>
